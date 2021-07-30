283adf7c5abb844c41eeacfd4b97dded435afb83
naskya <m@naskya.net>
2021-07-30 14:26:37 +0900

    Update document

--- include/algebra/pow.hpp
+++ include/algebra/pow.hpp
@@ -10,12 +10,29 @@ namespace lib {
 //! @tparam IntType index type (deduced from parameter).
 //! @param base base. This doesn't have to be an integer.
 //! @param index index. This must be an integer, but doesn't have to be primitive.
+//! @return the index-th power of base
+//! @note Time complexity: O(log(index) * (time needed to calculate (base * base)))
+template <typename ValueType, typename IntType>
+[[nodiscard]] ValueType pow(ValueType base, IntType index) {
+  ValueType res = 1;
+  while (index != 0) {
+    if ((index & 1) == 1)
+      res *= base;
+    base *= base;
+    index >>= 1;
+  }
+  return res;
+}
+
+//! @tparam ValueType base type (deduced from parameter).
+//! @tparam IntType index type (deduced from parameter).
+//! @param base base. This doesn't have to be an integer.
+//! @param index index. This must be an integer, but doesn't have to be primitive.
 //! @param id multiplicative identity
 //! @return the index-th power of base
 //! @note Time complexity: O(log(index) * (time needed to calculate (base * base)))
 template <typename ValueType, typename IntType>
-[[nodiscard]] ValueType pow(ValueType base, IntType index, ValueType id = 1) {
-  // Use id as result in this function
+[[nodiscard]] ValueType pow(ValueType base, IntType index, ValueType id) {
   while (index != 0) {
     if ((index & 1) == 1)
       id *= base;
